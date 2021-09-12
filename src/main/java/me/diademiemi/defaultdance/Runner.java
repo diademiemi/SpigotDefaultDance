4631dc0c985ee62440cb8b86f290319a8507b0f1
diademiemi <emilia@diademiemi.me>
2021-09-12 02:22:06 +0200

    Initial commit

---
+++ src/main/java/me/diademiemi/defaultdance/Runner.java
@@ -0,0 +1,20 @@
+package me.diademiemi.defaultdance;
+
+import org.bukkit.entity.Player;
+
+/**
+ * Runner for Default Dance
+ *
+ * @author : diademiemi
+ * @created : 2021-09-12
+**/
+public class Runner {
+	
+	/**
+	 * TEMPORARY
+	 */
+	public static void doDefaultDance(Player player) {
+		System.out.println("TEMPORARY");
+	}
+
+}
